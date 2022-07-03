73705615265ba0812d0762072ed6b222ea2cbf52
Pratiek Parashar <prateek.parashar730@gmail.com>
2022-07-03 20:53:11 +0530

    added the google SSO and secured the /secret endpoint for sing on

--- server.js
+++ server.js
@@ -3,43 +3,74 @@ const path = require('path')
 const https = require('https')
 const express = require('express')
 const helmet = require('helmet')
+const passport = require('passport')
+const { Strategy } = require('passport-google-oauth20')
+
 
 require('dotenv').config();
 const port = 3000;
-
-
-const app = express()
 
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
 }
 
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,   
+}
+
+var isLoggedIn = false;
+
+function verifyCallBack(accessToken, refreshToken, profile, done) //This is the access token I am asking about
+{
+    console.log('Google Profile', profile);
+    if(profile.__json.email == 'pratiek.parashar@gmail.com')
+        isLoggedIn = true;
+    
+    done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallBack) )
+
+const app = express()
+
+
+
+
+
 app.use(helmet());
+
+app.use(passport.initialize()) 
 
 function checkLoggedIn(req,res,next) 
 {    
-    const isLoggedIn = true;
-    // if is logged in it's true else false (its boolean)
-
     if(!isLoggedIn)
     {
         return res.status(401).json({
-            error: 'You must log in',
+            error: 'Not authorized',
         })
-    }
-    
+    }    
     next();
 }
 
-app.get('/auth/google', (req,res) => {
+app.get('/auth/google',passport.authenticate('google', {
+    scope: ['email']
+}))
 
-})
 
-
-app.get('/auth/google/callback', (req,res) => {
-
-})
+app.get('/auth/google/callback',
+    (req,res,next) => {
+        console.log('Google Called Us Back')
+        next();
+    }, 
+    passport.authenticate('google', {
+        failureRedirect: '/failure',
+        successRedirect: '/',
+        session: false,
+    })    
+);
     
 app.get('/auth/logout',(req,res) => {
 
@@ -51,6 +82,10 @@ app.get('/auth/logout',(req,res) => {
 app.get('/secret', checkLoggedIn, (req, res) => {
     return res.send('Personal value is 42!!');
 });
+
+app.get('/failure',  (req,res) => {
+    return res.send('Failed to log in!');
+})
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html' )))
 
